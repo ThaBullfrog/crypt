ad9ccc999dfa95aeafea832829a5459443260e9e
thabullfrog <th3bullfrog@gmail.com>
2017-07-05 16:53:20 -0400

    apply small fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,6 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_controller.relative_url_root = '/secret_program'
+  config.relative_url_root = "/app1"
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
 
   root 'application#hello'
-  get '/secret_program', to: 'application#hello'
 
 end
